cbec0d7e30b96ef32058344d3a959baaac574c7a
tsubakimoto <tkmbmbr@gmail.com>
2015-03-01 15:34:32 +0900

    add HomeController.cs

---
+++ MyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Begin/
+
+        public ActionResult Index()
+        {
+            ViewBag.Msg = "こんにちは！ ASP.NET MVC!!";
+            //ViewData["msg"] = "こんにちは！ ASP.NET MVC!!";
+            return View();
+        }
+
+    }
+}
